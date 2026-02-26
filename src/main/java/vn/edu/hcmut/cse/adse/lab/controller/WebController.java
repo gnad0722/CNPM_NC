faac19bcdabf8b4464db03159fa6734baa5a9465
gnad0722 <haidang25102005@gmail.com>
2026-02-26 11:19:50 +0700

    Deploy Completed

---
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/controller/WebController.java
@@ -0,0 +1,12 @@
+package vn.edu.hcmut.cse.adse.lab.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/")
+public class WebController {
+    @GetMapping
+    public String home(){
+        return "welcome";
+    }
+}
